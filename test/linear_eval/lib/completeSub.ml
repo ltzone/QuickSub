503026345054ece096b9ffcfd80f2775acd7bf45
ltzone <ltzhou@sjtu.edu.cn>
2023-02-11 17:48:08 +0800

    linear algorithm evaluation

---
+++ test/linear_eval/lib/completeSub.ml
@@ -0,0 +1,73 @@
+open Defs
+
+type cetyp = CENat | CEReal | CEFun of cetyp * cetyp
+           | CEVar of int | CEProd of cetyp * cetyp * bool
+           | CESum of cetyp * cetyp * bool
+
+
+let isUninhabited (t: cetyp) (u: bool array) : bool =
+  match t with
+  | CEProd (_, _, b) | CESum (_, _, b) -> b
+  | CEVar n -> Array.get u n
+  | _ -> false
+
+let rec init (t:typ) 
+             (ut: cetyp array)  (* unroll table *)
+             (u: bool array) (* value-uninhabitation flags *)
+             (b: bool) (* commit value-uninhabitation of previously seen recursive type or not *) =
+  match t with
+  | Nat -> CENat
+  | Real -> CEReal
+  | Fun (t1, t2) -> CEFun (init t1 ut u b, init t2 ut u b)
+  | Sum (t1, t2) -> 
+      let cet1 = init t1 ut u b in
+      let cet2 = init t2 ut u b in
+        CESum (cet1, cet2, isUninhabited cet1 u && isUninhabited cet2 u)
+  | Prod (t1, t2) ->
+      let cet1 = init t1 ut u b in
+      let cet2 = init t2 ut u b in
+        CEProd (cet1, cet2, isUninhabited cet1 u || isUninhabited cet2 u)
+  | Var n -> CEVar n
+  | Rec (n, t) ->
+      Array.set u n true;
+      Array.set u n (isUninhabited (init t ut u true) u);
+      if b then () else Array.set ut n (init t ut u false);
+      CEVar n
+
+let sub (t1: typ) (t2: typ) =
+  let m = numVars t1 in
+  let n = numVars t2 in
+  let ut1 = Array.make m CENat in
+  let ut2 = Array.make n CENat in
+  let u1 = Array.make m false in
+  let u2 = Array.make n false in
+  let cet1 = init t1 ut1 u1 false in
+  let cet2 = init t2 ut2 u2 false in
+  let s1 = Array.make_matrix m n false in
+  let s2 = Array.make_matrix n m false in
+  let rec subh (cet1, ut1, u1) (cet2, ut2, u2) (s1, s2) = 
+    if isUninhabited cet1 u1 (* S-bottom *)
+    then true else
+      if ( match cet2 with
+          | CEFun (cet2', _) -> isUninhabited cet2' u2
+          | _ -> false ) then true else
+      match (cet1, cet2) with
+      | (CENat, CENat) -> true (* S-Nat *)
+      | (CEReal, CEReal) -> true (* S-Real *)
+      | (CENat, CEReal) -> true (* S-Base *)
+      | (CEFun (t1, t2), CEFun (t1', t2')) -> (* S-Fun *)
+          subh (t1', ut2, u2) (t1, ut1, u1)  (s2, s1) &&
+          subh (t2, ut1, u1) (t2', ut2, u2) (s1, s2)
+      | (CEProd (t1, t2, _), CEProd (t1', t2', _)) -> (* S-Prod *)
+          subh (t1, ut1, u1) (t1', ut2, u2) (s1, s2) &&
+          subh (t2, ut1, u1) (t2', ut2, u2) (s1, s2)
+      | (CESum (t1, t2, _), CESum (t1', t2', _)) -> (* S-Sum *)
+          subh (t1, ut1, u1) (t1', ut2, u2) (s1, s2) &&
+          subh (t2, ut1, u1) (t2', ut2, u2) (s1, s2)
+      | (CEVar n, CEVar m) -> (* S-Rec *)
+          if s1.(m).(n) then true else
+            (s1.(m).(n) <- true;
+             subh (ut1.(m), ut1, u1) (ut2.(n), ut2, u2) (s1, s2))
+      | _ -> false
+  in
+    subh (cet1, ut1, u1) (cet2, ut2, u2) (s1, s2)
---
+++ test/linear_eval/lib/defs.ml
@@ -0,0 +1,45 @@
+type typ = Nat | Real | Prod of typ * typ | Sum of typ * typ 
+         | Fun of typ * typ | Rec of int * typ | Var of int
+
+
+let rec numVars (t: typ) = match t with 
+  | Prod (t1, t2) | Sum (t1, t2) | Fun (t1, t2) -> numVars t1 + numVars t2 
+  | Rec (_, t) -> numVars t + 1
+  | _ -> 0
+
+
+let range a b =
+    List.init (b - a) ((+) a)
+
+
+let rev_concat_map f xs = List.fold_right List.rev_append (List.rev_map f xs) []
+(* The tail-recursive version of concat map *)
+
+let rec typgenh (depth: int) (max_var: int) : typ list =
+  if depth = 0 then 
+      ([Nat; Real] @ (List.map (fun i -> Var i) (range 0 max_var)))
+  else
+    (let st1 = typgenh (depth - 1) max_var in
+      let st2 = typgenh (depth - 1) (max_var + 1) in
+        let st1_comb = rev_concat_map 
+            (fun t1 -> rev_concat_map (fun t2 -> [Prod (t1, t2); Sum (t1, t2); Fun (t1, t2)]) st1) st1 in
+        let st2_comb = rev_concat_map (fun t -> [Rec (max_var, t)]) st2 in
+        List.rev_append st1_comb st2_comb
+    )
+    
+let typgen (depth: int) = 
+    typgenh depth 0
+
+let ascii n = String.make 1 (Char.chr (Char.code 'a' + n))
+
+let rec string_of_typ t = match t with
+  | Nat -> "nat"
+  | Real -> "real"
+  | Prod (t1, t2) -> "(" ^ string_of_typ t1 ^ " * " ^ string_of_typ t2 ^ ")"
+  | Sum (t1, t2) -> "(" ^ string_of_typ t1 ^ " + " ^ string_of_typ t2 ^ ")"
+  | Fun (t1, t2) -> "(" ^ string_of_typ t1 ^ " -> " ^ string_of_typ t2 ^ ")"
+  | Rec (i, t) -> "(μ " ^ ascii i ^ ". " ^ string_of_typ t ^ ")"
+  | Var i -> ascii i
+
+let print_typ t = print_string (string_of_typ t)
+
