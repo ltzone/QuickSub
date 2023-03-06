db42d9a01ae47cfa1a61c5b5f2a15f6c38019988
ltzone <ltzhou@sjtu.edu.cn>
2023-03-06 11:26:06 +0800

    amber rules implementation

--- test/linear_eval/bin/main.ml
+++ test/linear_eval/bin/main.ml
@@ -1,5 +1,5 @@
-let main = 
-  List.iter Tests.test6 [1;10;50;100;200;500;1000;2000;3000;4000;5000;6000;7000;8000;9000;10000]
+(* let main = 
+  List.iter Tests.test6 [1;10;50;100;200;500;1000;2000;3000;4000;5000;6000;7000;8000;9000;10000] *)
 
 (* let t1, t2 = Defs.deep_subtyp_gen 2 Nat Real in
 LinearSubOpt.sub t1 t2 *)
@@ -7,13 +7,13 @@ LinearSubOpt.sub t1 t2 *)
 
 (* Test random *)
 
-(* let main = 
+let main = 
   let tys = Defs.typ_pair_gen 4 in
   List.iter (
     fun (t1, t2) -> 
-      let res1 = LinearSubOpt.sub t1 t2 in
+      let res1 = AmberSub.sub t1 t2 in
       let res2 = LinearSub.sub t1 t2 in
       if res1 <> res2 then
-        Printf.printf "Error: %s <: %s, \t optimized:%s \t expected:%s" (Defs.string_of_typ t1) (Defs.string_of_typ t2) (string_of_bool res1) (string_of_bool res2)
-  ) tys *)
+        Printf.printf "Error: %s <: %s, \t Amber:%s \t Linear:%s\n" (Defs.string_of_typ t1) (Defs.string_of_typ t2) (string_of_bool res1) (string_of_bool res2)
+  ) tys
 
---
+++ test/linear_eval/lib/amberSub.ml
@@ -0,0 +1,62 @@
+
+(* Implemented for named index, with subsitution *)
+
+open Defs;;
+
+type env = (int * int) list
+
+let rec wf_type (e: env) (x:typ) (i: int) : bool =
+  match x with
+  | Nat -> true
+  | Real -> true
+  | Top -> true
+  | Fun (a, b) -> wf_type e a i && wf_type e b i
+  | Prod (a, b) -> wf_type e a i && wf_type e b i
+  | Sum (a, b) -> wf_type e a i && wf_type e b i
+  | Var i -> List.mem_assoc i e || List.mem i (List.map snd e)
+  | Rec (j, a) -> 
+      let i' = i + 1 in
+      let e' = (j, i') :: e in
+      wf_type e' a i'
+
+let rec subst_var (i: int) (x: typ) (y: typ) : typ =
+  match y with
+  | Nat -> Nat
+  | Real -> Real
+  | Top -> Top
+  | Fun (a, b) -> Fun (subst_var i x a, subst_var i x b)
+  | Prod (a, b) -> Prod (subst_var i x a, subst_var i x b)
+  | Sum (a, b) -> Sum (subst_var i x a, subst_var i x b)
+  | Var j when i = j -> x
+  | Var j -> Var j
+  | Rec (j, a) when i = j -> Rec (j, a)
+  | Rec (j, a) ->
+      Rec (j, subst_var i x a)
+
+let rec subh (i: int) (e:env) (x:typ) (y:typ) : bool =
+  match (x, y) with
+  | (Nat, Nat) -> true
+  | (Real, Real) -> true
+  | (Nat, Real) -> true
+  | (a, Top) -> wf_type e a i
+  | (Fun (a1, a2), Fun (b1, b2)) ->
+      subh i e b1 a1 && subh i e a2 b2
+  | (Prod (a1, a2), Prod (b1, b2)) ->
+      subh i e a1 b1 && subh i e a2 b2
+  | (Sum (a1, a2), Sum (b1, b2)) ->
+      subh i e a1 b1 && subh i e a2 b2
+  | (Var i, Var j) -> List.mem (i, j) e
+  | (Rec (j, a), Rec (k, b)) when j = k  && a = b -> wf_type e (Rec (j, a)) i
+  | (Rec (j, a), Rec (k, b)) when j = k ->
+      let j' = i + 1 in
+      let k' = i + 2 in
+      let e' = (j', k') :: e in
+      subh k' e' (subst_var j (Var j') a) (subst_var k (Var k') b)
+  | _, _ -> false
+
+
+
+  
+let sub (x:typ) (y:typ) : bool =
+  let fresh_i = max (numVars x) (numVars y) in
+  subh fresh_i [] x y
