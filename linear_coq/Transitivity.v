73e1eb65fc0857b83958162502c67aa2e2bed805
ltzone <ltzhou@sjtu.edu.cn>
2023-03-07 00:40:05 +0800

    meeting 0307

--- linear_coq/Transitivity.v
+++ linear_coq/Transitivity.v
@@ -24,6 +24,92 @@ Qed.
 
 
 Lemma trans_aux: forall B E,
+    type B -> forall im A C evs1 evs2 cm1 cm2,
+    Sub im cm1 evs1 E A B -> Sub im cm2 evs2 E B C -> 
+    evs1 [=] emp -> evs2 [=] emp ->
+    exists cm', Sub im cm' emp E A C.
+Proof with auto.
+  intros B E H. revert E.
+  dependent induction H;intros;try solve [solve_by_inv H0 H|solve_by_inv H H0|solve_by_inv H0 H2]...
+  - admit.
+  - admit.
+  - admit.
+  - admit.
+  -
+    dependent induction H1;subst...
+    +
+      dependent induction H3;subst...
+      *
+        admit.
+      *
+        clear H2 H4.
+        exists Lt.
+        apply Sa_rec_lt with (L:=L \u L0 \u L1)...
+        intros. specialize_x_and_L X L1.
+        specialize_x_and_L X L0.
+        specialize_x_and_L X L.
+        destruct H0 with (im:=im) (A:=open_tt A1 X) (C:=open_tt A2 X)
+        (evs1:=evs) (evs2:=evs0) (cm1:=Lt) (cm2:=Lt)
+        (E:= X ~ bind_sub im ++ E)... destruct x...
+        admit.
+      * clear H2 H4.
+Admitted.
+
+
+Lemma trans_aux': forall B E,
+    WFS E B -> forall im A C evs1 evs2,
+    Sub im Lt evs1 E A B -> Sub im Lt evs2 E B C -> 
+    evs1 [=] emp -> evs2 [=] emp ->
+    Sub im Lt emp E A C.
+Proof with auto.
+  intros B E H.
+  dependent induction H;intros;try solve [solve_by_inv H0 H|solve_by_inv H H0|solve_by_inv H0 H2]...
+  -
+    (* B = Top *)
+    dependent induction H0;subst...
+    { exfalso... }
+    { rewrite <- H1 in *... }
+  -
+    (* B = Nat *)
+    dependent induction H0;subst...
+    +
+      (* A <: nat <: Top *)
+      dependent induction H;subst...
+      { rewrite <- H0 in *... }
+    +
+      rewrite <- H1 in *...
+  -
+    (* B = X *)
+    dependent induction H1;subst...
+    +
+      (* A <:  X <: Top *)
+      dependent induction H0;subst...
+      { rewrite <- H4 in *... apply IHSub with (X0:=X)... }
+    +
+      rewrite <- H2 in *... apply IHSub with (X0:=X)...
+  -
+    (* B = A -> B *)
+    dependent induction H2;subst...
+    +
+      (* A0 < A -> B < Top *)
+      dependent induction H1;subst...
+      *
+        (* A1 -> A2 < A -> B < Top *)
+        apply Sa_top_lt...
+        { get_well_form... } { intros C. inversion C. }
+      *
+        rewrite <- H5 in *... apply IHSub with (B0:=B) (A0:=A)...
+    +
+      (* A0 < A -> B < B1 -> B2 *)
+      dependent induction H1;subst...
+      *
+        (* A1 -> A2 < A -> B < B1 -> B2 *)
+        clear IHSub1. clear IHSub2. clear IHSub3. clear IHSub0.
+        destruct cm0, cm3; try solve [inversion H1].
+Admitted.
+
+
+Lemma trans_aux2: forall B E,
     WFS E B -> forall im cm1 cm2 A C evs1 evs2,
     Sub im cm1 evs1 E A B -> Sub im cm2 evs2 E B C -> 
     exists evs, evs [<=] evs1 `union` evs2 /\
@@ -161,4 +247,27 @@ Proof with auto.
           (* require syntactic *) admit.
           
         }
-    
+      *
+        admit.
+    + admit.
+
+  - 
+    dependent induction H1.
+    +
+      (* Lt *)
+      dependent induction H3;subst...
+      *
+        (* Rec Lt + Top *)
+        admit.
+      *
+        (* Rec Lt + Lt *)
+        clear H4 H2.
+        pick_fresh X.
+        specialize_x_and_L X L.
+        specialize_x_and_L X L0.
+        specialize_x_and_L X L1.
+        (* need to induction on type B *)
+        admit.
+      *
+        
+
